892abf071fd4f446672358317ecb72b3e5296afb
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2021-07-23 22:21:01 +0300

    feat: add GussingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.mim = min;
     }
 
     guess() {
-
+       return this.result = Math.ceil((this.max + this.min) / 2);
     }
 
     lower() {
-
+        return this.max = this.result;
     }
 
     greater() {
-
+       return this.min = this.result;
     }
 }
 
